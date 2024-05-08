16b5b180119ce1764de29cd03fcf347c6690f0d0
Jamshed Damkewala <jamshedd@users.noreply.github.com>
2024-05-08 03:41:45 -0700

    Update CustomTabbedPage.cs

--- src/ClientApp/Controls/CustomTabbedPage.cs
+++ src/ClientApp/Controls/CustomTabbedPage.cs
@@ -2,7 +2,6 @@
 
 public class CustomTabbedPage : TabbedPage
 {
-
     public static BindableProperty BadgeTextProperty =
         BindableProperty.CreateAttached("BadgeText", typeof(string), typeof(CustomTabbedPage), default(string));
 
